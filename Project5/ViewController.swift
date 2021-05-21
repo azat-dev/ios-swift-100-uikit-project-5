cd4634dedd9667d38969797bc8204d250232941c
Azat <kaumov.a.r@gmail.com>
2021-05-21 13:49:44 +0300

    Add the answer dialog

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -22,17 +22,66 @@ class ViewController: UITableViewController {
         }
         
         allWords = startWords.components(separatedBy: "\n")
+        
         if allWords.isEmpty {
             allWords = ["silkworm"]
         }
         
         print("Words were loaded \(allWords.count)")
     }
-
+    
+    func initNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(promptForAnswer)
+        )
+    }
+    
+    func submit(word: String) {
+        
+    }
+    
+    @objc func promptForAnswer() {
+        let alert = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        alert.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak alert] _ in
+            guard let word = alert?.textFields?[0].text else {
+                return
+            }
+            
+            self?.submit(word: word)
+        }
+        
+        alert.addAction(submitAction)
+        present(alert, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadWords()
+        initNavigationBar()
+        startGame()
+    }
+    
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        
+        return cell
     }
 }
 
