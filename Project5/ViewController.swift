e4229da0440355da81403c28ab2a1f692c97552d
Azat <kaumov.a.r@gmail.com>
2021-05-21 12:42:19 +0300

    Add ability to load words from the file

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    func loadWords() {
+        guard let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt") else {
+            return
+        }
+        
+        guard let startWords = try? String(contentsOf: startWordsUrl) else {
+            return
+        }
+        
+        allWords = startWords.components(separatedBy: "\n")
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        print("Words were loaded \(allWords.count)")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadWords()
     }
-
-
 }
 
